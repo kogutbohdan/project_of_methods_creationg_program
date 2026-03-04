0fbc88b61fb0789782d322fba845a20089ebb143
kohutbohdan <bohdan.v.kohut@ukd.edu.ua>
2026-03-04 22:33:44 +0200

    beckend-begin

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from sentence_transformers import SentenceTransformer
+
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "FastAPI працює 🚀"}
+    return "Hello World"
+
+@app.post("/query")
+def complete_query(query:str):
+    sentens_transformer=SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+    print(sentens_transformer.encode([query]))
+    return {"ok":True}
